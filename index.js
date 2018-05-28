b3af1a7f33d1aca8284e0c369f9686f183b8dcfe
Per Djurner <per.djurner@gmail.com>
2018-05-28 20:18:31 +0200

    Create server.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Create server.
+const app = express();
+
+// CORS.
+app.use(cors());
+app.options('*', cors());
+
+// Body parser.
+app.use(bodyParser.json());
+
+// Serve static files.
+app.use(express.static('public'));
+
+// Start server.
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Running on port ${port}.`);
+});
